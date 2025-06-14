d55107e0589f979370ed75b452fc3e0db8b264af
wenfeng <florentxingwenfeng@gmail.com>
2025-06-14 12:56:24 -0400

    fix base path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-export const basePath = "profile"
+export const basePath = process.env.NODE_ENV === "development" ? "" : "/profile"
 
 const nextConfig: NextConfig = {
   basePath: basePath,
-  assetPrefix: `/${basePath}/`,
+  assetPrefix: `${basePath}/`,
 };
 
 export default nextConfig;
